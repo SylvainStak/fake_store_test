9abd1a8c5c663c0fad67b5a8ae0deb31fbf418b0
SylvainStak <stackersap@gmail.com>
2021-10-17 18:58:38 +0100

    added product info

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -4,9 +4,12 @@ import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import { useQuery } from 'react-query';
+import Grid from '@mui/material/Grid';
 import axios from 'axios';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import CircularProgress from '@mui/material/CircularProgress';
+import Rating from '@mui/material/Rating';
+import PersonIcon from '@mui/icons-material/Person';
 import '../styles/ProductInfo.css';
 
 function ProductInfo(props) {
@@ -29,8 +32,25 @@ function ProductInfo(props) {
               <CircularProgress />
           </div>          
           ) : (
-          <Container maxWidth="md" className="product_info_container">
-            {data.data.title}
+          <Container maxWidth="xs" className="product_info_container">
+            <h2 className="product_title">{data.data.title}</h2>
+            <h4 className="product_category">Category: {data.data.category}</h4>
+            <div className="product_image_container">
+              <img src={data.data.image} className="product_image" width="300" height="300" alt={data.data.alt} />
+            </div>
+            <Grid container spacing={2} className="product_showcase_container">
+              <Grid item md={4} sm={12} xs={12}>
+                <h2>$ {data.data.price}</h2>
+              </Grid>
+              <Grid item md={4} sm={12} xs={12}>
+                
+              </Grid>
+              <Grid item md={4} sm={12} xs={12}>
+                <Rating name="read-only" value={data.data.rating.rate} precision={0.1} readOnly />
+                {data.data.rating.count} <PersonIcon className="reviewIcon" />
+              </Grid>
+            </Grid>
+            <p className="product_description">{data.data.description}</p>
           </Container>
           )}
         </>
