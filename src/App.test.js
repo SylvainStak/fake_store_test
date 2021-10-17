fdb97b15f0589c2baeace7e5bed53592a482334a
SylvainStak <stackersap@gmail.com>
2021-10-17 19:21:07 +0100

    modify test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders titles', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const categoriesTitle = screen.getByText(/Categories/i);
+  const productsTitle = screen.getByText(/Products/i);
+  expect(categoriesTitle).toBeInTheDocument();
+  expect(productsTitle).toBeInTheDocument();
 });
